8c00e2eaf40308e1bdaa706a64f7fbc687ef125d
SAQG007 <syedabdulqadirgillani807@gmail.com>
2023-08-23 17:00:29 +0500

    Created choose screen

---
+++ lib/pages/choose.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:rive/rive.dart';
+import 'package:tic_tac_toe/global/utils.dart';
+import 'package:tic_tac_toe/pages/board.dart';
+import 'package:tic_tac_toe/widgets/menu_button.dart';
+
+class Choose extends StatefulWidget {
+  const Choose({ Key? key }) : super(key: key);
+
+  @override
+  _ChooseState createState() => _ChooseState();
+}
+
+class _ChooseState extends State<Choose> {
+
+  void _navigateToBoard() {
+    playButtonTapSound();
+    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const Board()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          const RiveAnimation.asset(
+            'assets/backgrounds/planet-orbit.riv',
+            fit: BoxFit.cover,
+            alignment: Alignment.center,
+            antialiasing: true,
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              MenuButton(title: "Play as X", onTap: _navigateToBoard,),
+              MenuButton(title: "Play as O", onTap: _navigateToBoard,),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
