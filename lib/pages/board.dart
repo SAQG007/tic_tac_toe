e0cda772fe8ed036b5f48abb71db9049113611ec
SAQG007 <syedabdulqadirgillani807@gmail.com>
2023-08-24 16:33:36 +0500

    Player change functionality

--- lib/pages/board.dart
+++ lib/pages/board.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tic_tac_toe/global/utils.dart';
 
 class Board extends StatefulWidget {
   final String symbol;
@@ -13,11 +14,37 @@ class Board extends StatefulWidget {
 }
 
 class _BoardState extends State<Board> {
+
+  String currentPlayer = "";
+  List<String> gridSymbols = List.filled(9, "");
+  int _filledIndexCount = 0;
+
+  void _changeGridSymbol(int index) {
+    setState(() {
+      gridSymbols[index] = currentPlayer;
+    });
+    _filledIndexCount++;
+  }
+
+  void _changeCurrentPlayer() {
+    setState(() {
+      currentPlayer == "X" ? currentPlayer = "O" : currentPlayer = "X";
+    });
+  }
+
+  void _resetBoard() {
+    setState(() {
+      gridSymbols = List.filled(9, "");
+    });
+    _filledIndexCount = 0;
+  }
+
   @override
   void initState() {
     super.initState();
-    print("The symbol is: ${widget.symbol}");
+    currentPlayer = widget.symbol;
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,7 +52,7 @@ class _BoardState extends State<Board> {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Text(
-            "Your Turn (X)",
+            "Your Turn ($currentPlayer)",
             style: Theme.of(context).textTheme.displaySmall,
           ),
           Center(
@@ -34,25 +61,33 @@ class _BoardState extends State<Board> {
               padding: const EdgeInsets.fromLTRB(10.0, 35.0, 10.0, 10.0),
               crossAxisCount: 3,
               children: List.generate(9, (index) {
-                return Container(
-                  margin: const EdgeInsets.all(6.0),
-                  decoration: const BoxDecoration(
-                    color: Colors.amber,
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(20.0)
-                    ),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black26,
-                        blurRadius: 5.0,
-                        offset: Offset(2, 2),
+                return GestureDetector(
+                  onTap: () {
+                    playButtonTapSound();
+                    _changeGridSymbol(index);
+                    _changeCurrentPlayer();
+                    print("Filled index count: $_filledIndexCount");
+                  },
+                  child: Container(
+                    margin: const EdgeInsets.all(6.0),
+                    decoration: const BoxDecoration(
+                      color: Colors.amber,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(20.0)
                       ),
-                    ],
-                  ),
-                  child: Center(
-                    child: Text(
-                      '$index',
-                      style: Theme.of(context).textTheme.headlineLarge,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.white60,
+                          blurRadius: 5.0,
+                          offset: Offset(2, 2),
+                        ),
+                      ],
+                    ),
+                    child: Center(
+                      child: Text(
+                        gridSymbols[index],
+                        style: Theme.of(context).textTheme.headlineLarge,
+                      ),
                     ),
                   ),
                 );
@@ -62,7 +97,10 @@ class _BoardState extends State<Board> {
         ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          playButtonTapSound();
+          _resetBoard();
+        },
         child: const Icon(Icons.replay_outlined),
       ),
     );
